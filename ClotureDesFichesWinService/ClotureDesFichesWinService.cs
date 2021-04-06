88692bcdf34ad9673984ebabc3a263d01d0bc4d5
Zelda4 <78208983+Zelda4@users.noreply.github.com>
2021-04-06 22:45:25 +0200

    Add files via upload

---
+++ ClotureDesFichesWinService/ClotureDesFichesWinService.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace ClotureDesFichesWinService
+{
+    public partial class ClotureDesFichesWinService : ServiceBase
+    {
+        //création d'une variable timer
+        private Timer syncTimer = null;
+
+        //création d'une variable path
+        string filepath = Path.Combine(Path.GetDirectoryName
+                          (new Uri(Assembly.GetExecutingAssembly()
+                            .CodeBase).LocalPath), "Clôture des fiches.exe");
+
+
+        public ClotureDesFichesWinService()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            syncTimer = new Timer();
+            this.syncTimer.Interval = 600000; //10 min
+            this.syncTimer.Elapsed +=
+                new System.Timers.
+                   ElapsedEventHandler(this.syncTimerTicker);
+            syncTimer.Enabled = true;
+        }
+
+        protected override void OnStop()
+        {
+            syncTimer.Enabled = false;
+        }
+
+        
+        private void syncTimerTicker(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start(filepath);
+        }
+    }
+}
---
+++ ClotureDesFichesWinService/ProjectInstaller.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace ClotureDesFichesWinService
+{
+    partial class ProjectInstaller
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur de composants
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ClotureDesFichesWinServiceProcess = new System.ServiceProcess.ServiceProcessInstaller();
+            this.ClotureDesFichesWinService = new System.ServiceProcess.ServiceInstaller();
+            // 
+            // ClotureDesFichesWinServiceProcess
+            // 
+            this.ClotureDesFichesWinServiceProcess.Account = System.ServiceProcess.ServiceAccount.LocalSystem;
+            this.ClotureDesFichesWinServiceProcess.Password = null;
+            this.ClotureDesFichesWinServiceProcess.Username = null;
+            // 
+            // ClotureDesFichesWinService
+            // 
+            this.ClotureDesFichesWinService.DisplayName = "ClotureDesFichesWinService";
+            this.ClotureDesFichesWinService.ServiceName = "ClotureDesFichesWinService";
+            this.ClotureDesFichesWinService.StartType = System.ServiceProcess.ServiceStartMode.Automatic;
+            // 
+            // ProjectInstaller
+            // 
+            this.Installers.AddRange(new System.Configuration.Install.Installer[] {
+            this.ClotureDesFichesWinServiceProcess,
+            this.ClotureDesFichesWinService});
+
+        }
+
+        #endregion
+
+        private System.ServiceProcess.ServiceProcessInstaller ClotureDesFichesWinServiceProcess;
+        private System.ServiceProcess.ServiceInstaller ClotureDesFichesWinService;
+    }
+}
